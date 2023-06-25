371592f76a0e917a7eddac749be7d46cf4bb070c
edudias1972 <edudias1972@gmail.com>
2023-06-24 23:08:29 -0300

    Hello GoLang

--- hello.go
+++ hello.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("hello, world\nEduardo Maurício Dias\n")
+	fmt.Printf("GoLang programando em go\n")
+	fmt.Printf("Multiplataforma\n")
+	fmt.Printf("O que é golang?\n\n")
+	fmt.Printf("Linguagem de programação open source que tem objetivo \n de tornar os programadores mais produtivos \n")
+	fmt.Printf("Expressiva ,concisa e limpa , eficiente\n")
+	fmt.Printf("Criada para aproveitar o máximo\n dos recursos multicore e de rede\n")
+	fmt.Printf("Rápida compilação e ao mesmo \n tempo trabalha com garbage collection \n")
+	fmt.Printf("Rápida e dinamica, tipada, compilada \n")
+	fmt.Printf("Compilada em apenas um arquivo binário\n")
 }
